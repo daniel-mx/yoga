4e6351ed4191e6970006027395b41d253c1b19f5
Daniel <developvxz@gmail.com>
2020-05-19 20:11:38 +0300

    add slider & calc

---
+++ js/script2.js
@@ -0,0 +1,288 @@
+// табы
+window.addEventListener('DOMContentLoaded', () => {
+
+    'use strict';
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
+    }
+    hideTabContent(1);
+
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
+    }
+
+    info.addEventListener('click', (event) => {
+        let target = event.target;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (let i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+
+    });
+
+    // таймер
+    let deadline = '2020-05-07';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60))) - 3;
+
+        return {
+            "total": t,
+            "hours": hours,
+            "minutes": minutes,
+            "seconds": seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return '0' + num;
+                } else {
+                    return num;
+                }
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+    setClock('timer', deadline);
+
+    // modal
+
+    let more = document.querySelector('.more'),
+        descrBtn = document.querySelectorAll('.description-btn'),
+        overlay = document.querySelector('.overlay'),
+        close = document.querySelector('.popup-close');
+
+
+    [].forEach.call(descrBtn, function (itclass) {
+        itclass.addEventListener('click', function () {
+            overlay.style.display = "block";
+            this.classList.add('more-splash');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    more.addEventListener('click', function () {
+        overlay.style.display = "block";
+        this.classList.add('more-splash');
+        document.body.style.overflow = 'hidden';
+
+    });
+
+    close.addEventListener('click', () => {
+        overlay.style.display = "none";
+        more.classList.remove('more-splash');
+        document.body.style.overflow = '';
+    });
+
+    // form
+    let message = {
+        loading: 'Загрузка',
+        sucsess: 'Спасибо, скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так'
+    };
+
+    let form = document.querySelector('.main-form'),
+        input = form.getElementsByTagName('input'),
+        contactsForm = document.getElementById('form'),
+        contactsInput = contactsForm.getElementsByTagName('input'),
+        statusMessage = document.createElement('div');
+
+    statusMessage.classList.add('status');
+
+    function formSubmit(itform) {
+        itform.addEventListener('submit', function (event) {
+            event.preventDefault();
+            itform.appendChild(statusMessage);
+
+            let formData = new FormData(itform);
+
+            function postData(data) {
+                return new Promise(function (resolve, reject) {
+                    let request = new XMLHttpRequest();
+                    request.open('POST', 'server.php');
+                    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+                    // в json формате:   request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+
+                    request.onreadystatechange = function () {
+                        if (request.readyState < 4) {
+                            resolve();
+                        } else if (request.readyState === 4 && request.status == 200) {
+                            resolve();
+                        } else {
+                            reject();
+                        }
+                    };
+
+                    request.send(data);
+                    // В json формате: request.send(json);
+                });
+            }
+            // end postData
+
+            function clearInput() {
+                for (let i = 0; i < input.length; i++) {
+                    input[i].value = '';
+
+                }
+                for (let j = 0; j < contactsInput.length; j++) {
+                    contactsInput[j].value = '';
+                }
+            }
+
+            postData(formData)
+                .then(() => statusMessage.innerHTML = message.loading)
+                .then(() => {
+                    //thanksModal.style.display = "block";
+                    //mainModal.style.display = "none";
+                    statusMessage.innerHTML = message.sucsess;
+                    //statusMessage.innerHTML = "";
+                })
+                .catch(() => statusMessage.innerHTML = message.failure)
+                .then(clearInput);
+        });
+    }
+    formSubmit(form);
+    formSubmit(contactsForm);
+
+    // slider
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    showSlides(slideIndex);
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = "none");
+        dots.forEach((item) => item.classList.remove('dot-active'));
+        slides[slideIndex - 1].style.display = 'block';
+        dots[slideIndex - 1].classList.add('dot-active');
+    }
+
+    function plusSlides(n) {
+       showSlides(slideIndex += n);
+    }
+    function currentSlide(n) {
+        showSlides(slideIndex = n);
+    }
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    });
+
+    // calc
+
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personsSum = 0,
+        daysSum = 0,
+        total = 0;
+
+        
+
+        function countSum() {
+            totalValue.innerHTML = 0;
+            persons.addEventListener('change', function() {
+                personsSum = +this.value;
+                total = (daysSum + personsSum)*4000;
+    
+                if (restDays.value == '' ) {
+                    totalValue.innerHTML = 0;
+                } else {
+                    totalValue.innerHTML = total;
+                }
+                if (daysSum == 0 || personsSum == 0) {
+                    totalValue.innerHTML = 0;
+                }
+            });
+    
+            restDays.addEventListener('change', function() {
+                daysSum = +this.value;
+                total = (daysSum + personsSum)*4000;
+    
+                if (persons.value == '' || restDays.value == 0 || persons.value == 0) {
+                    totalValue.innerHTML = 0;
+                } else {
+                    totalValue.innerHTML = total;
+                } 
+                if (daysSum == 0 || personsSum == 0) {
+                    totalValue.innerHTML = 0;
+                }
+            });
+    
+            place.addEventListener('change', function() {
+                if (restDays.value == '' || persons.value == '' ) {
+                    totalValue.innerHTML = 0;
+                } else {
+                    let a = total;
+                    totalValue.innerHTML = a* this.options[this.selectedIndex].value;
+                }
+            });
+        }
+        countSum();
+        
+        // setInterval(() => {
+        //     if (restDays.value == '' || persons.value == '') {
+        //         totalValue.innerHTML = 0;  
+        //     }
+        // }, 50);
+});
